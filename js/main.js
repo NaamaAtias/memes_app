5afc527d0c4f1d0cd00b558b2eee8610609eefd6
Naama Atias <naamazch@gmail.com>
2021-10-27 15:32:46 +0300

    added grid gallery

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 'use strict'
 
 function init() {
+    createImgs();
+    renderImgs();
     renderGalleryPage();
     renderEditorPage();
 }
--- js/services/memes-service.js
+++ js/services/memes-service.js
@@ -1,29 +1,42 @@
 'use strict'
 
 // var gKeywords = {'happy': 12,'funny puk': 1}
-var imgId = 1;
-var gImgs = createImgs();
+var imgId = 0;
+var gImgs=[];
 
 function createImgs() {
-    gImgs = [
-        {id: imgId++, url:`img/memes-square/${imgId}.jpg`, keywords: []}
-    ]
+    for (var i = 0; i < 18; i++) {
+        gImgs.push(createImg());
+    }
+    console.log(gImgs);
 }
 
-function renderImgs() {
+function createImg() {
+    const currImg = { id: ++imgId, url: `img/memes-square/${imgId}.jpg`, keywords: [] };
+    return currImg;
+}
 
+
+function renderImgs() {
+    // getImgs()
+    let strHtml = '';
+    gImgs.forEach(currImg => {
+        strHtml += `<img class="grid-item" src=${currImg.url}>`
+    })
+    console.log(strHtml);
+    document.querySelector('.grid-gallery').innerHTML = strHtml;
 }
 
 
 var gMeme = {
- selectedImgId: 5,
- selectedLineIdx: 0,
- lines: [
- {
- txt: 'I never eat Falafel',
- size: 20,
- align: 'left',
- color: 'red'
- }
- ]
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I never eat Falafel',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
+    ]
 }
